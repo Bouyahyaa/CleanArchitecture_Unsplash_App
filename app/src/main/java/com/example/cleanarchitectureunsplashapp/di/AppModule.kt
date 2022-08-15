c62f7891479ca9eedc9ddb4bcb417d0663692772
Bilel_Bouyahyaa <bilel.bouyahya@outlook.com>
2022-08-15 17:41:18 +0100

    remove room provider

--- app/src/main/java/com/example/cleanarchitectureunsplashapp/di/AppModule.kt
+++ app/src/main/java/com/example/cleanarchitectureunsplashapp/di/AppModule.kt
@@ -1,9 +1,6 @@
 package com.example.cleanarchitectureunsplashapp.di
 
-import android.app.Application
-import androidx.room.Room
 import com.example.cleanarchitectureunsplashapp.core.Constants
-import com.example.cleanarchitectureunsplashapp.data.local.UnsplashDatabase
 import com.example.cleanarchitectureunsplashapp.data.remote.PictureApi
 import com.example.cleanarchitectureunsplashapp.data.repository.PictureRepositoryImpl
 import com.example.cleanarchitectureunsplashapp.domain.repository.PictureRepository
@@ -18,20 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Provides
-    @Singleton
-    fun providePictureDatabase(app: Application): UnsplashDatabase {
-        return Room.databaseBuilder(
-            app, UnsplashDatabase::class.java, UnsplashDatabase.DATABASE_NAME
-        ).build()
-    }
-
-//    @Provides
-//    @Singleton
-//    fun provideNoteRepository(db: UnsplashDatabase): PictureRepository {
-//        return PictureRepositoryImpl(db.unsplashDao)
-//    }
 
     @Provides
     @Singleton
